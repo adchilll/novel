8caa485c54af1c734b526d07c0511227f396977b
adchilll <9818186564d@gmail.com>
2026-05-21 14:32:54 +0300

    Create engine.js

---
+++ engine.js
@@ -0,0 +1,43 @@
+let scenes = {};
+let currentScene = "start";
+
+async function loadScenes(){
+
+    const response = await fetch("scenes.json");
+    scenes = await response.json();
+
+    showScene(currentScene);
+}
+
+function showScene(id){
+
+    const scene = scenes[id];
+
+    document.getElementById("background").src =
+        "assets/" + scene.background;
+
+    document.getElementById("character").src =
+        "assets/" + scene.character;
+
+    document.getElementById("text").innerText =
+        scene.text;
+
+    const choicesDiv = document.getElementById("choices");
+    choicesDiv.innerHTML = "";
+
+    scene.choices.forEach(choice => {
+
+        const button = document.createElement("button");
+
+        button.innerText = choice.text;
+
+        button.onclick = () => {
+            showScene(choice.next);
+        };
+
+        choicesDiv.appendChild(button);
+
+    });
+}
+
+loadScenes();
